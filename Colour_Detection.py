feb63cb7225d13715a938c503b044b250466b79d
aniketbharti <51299179+aniketbharti@users.noreply.github.com>
2022-04-22 11:18:53 -0400

    changes done

--- Colour_Detection.py
+++ Colour_Detection.py
@@ -1,7 +1,7 @@
-#Author: Karan Shah
-#Date:2/16/2022
-#University: University at Buffalo, The State University of New York
-#Rubik's Race Colour Detection 
+# Author: Karan Shah
+# Date:2/16/2022
+# University: University at Buffalo, The State University of New York
+# Rubik's Race Colour Detection
 
 #import pacakges
 import black
@@ -9,112 +9,122 @@ import cv2
 from cv2 import inRange
 import numpy as np
 import time
-import pygame, sys, random
+import pygame
+import sys
+import random
 from pygame.locals import *
 
-hsv = []; color_tile9 = []; color_tile25 = []; color_state = []
+hsv = []
+color_tile9 = []
+color_tile25 = []
+color_state = []
 
-tile_25  = {
-    'tile':['white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white','white'],
+tile_25 = {
+    'tile': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white'],
 }
 
 tile_9 = {
-    'tile':['white','white','white','white','white','white','white','white','white'],
+    'tile': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white'],
 }
 
 color = {
-        'white'  : (255,255,255),
-        'blue'   : (255,0,0),
-        'green'  : (0,255,0),
-        'yellow' : (0,255,255),
-        'red'    : (0,0,255),
-        'orange' : (0,165,255),
-        'black' : (0, 0, 0),
-        }
+    'white': (255, 255, 255),
+    'blue': (255, 0, 0),
+    'green': (0, 255, 0),
+    'yellow': (0, 255, 255),
+    'red': (0, 0, 255),
+    'orange': (0, 165, 255),
+    'black': (0, 0, 0),
+}
 
 stickers = {
-        # 'tile_9': [
-        #     [140, 130], [240, 130], [340, 130],
-        #     [140, 230], [240, 230], [340, 230],
-        #     [140, 330], [240, 330], [340, 330]
-        # ],
-        'tile_9': [
-            [370, 210], [460, 210], [550, 210],
-            [370, 290], [460, 290], [550, 290],
-            [370, 380], [460, 380], [550, 380]
-        ],
-        'prev_9': [
-            [10, 10], [30, 10], [50, 10],
-            [10, 30], [30, 30], [50, 30],
-            [10, 50], [30, 50], [50, 50]
-        ],
-        # 'tile_25': [
-        #     [200, 80], [280, 80], [360, 80], [440, 80], [520, 80],
-        #     [200, 155], [280, 155], [360, 155], [440, 155], [520, 155],
-        #     [200, 230], [280, 230], [360, 230], [440, 230], [520, 230],
-        #     [200, 305], [280, 305], [360, 305], [440, 305], [520, 305],
-        #     [200, 380], [280, 380], [360, 380], [440, 380], [520, 380],
-        # ],
-            'tile_25': [
-            [130, 80], [270, 80], [395, 80], [500, 80], [620, 80],
-            [130, 180], [270, 180], [395, 180], [500, 180], [620, 180],
-            [130, 290], [270, 290], [395, 290], [500, 290], [620, 290],
-            [130, 410], [270, 410], [395, 410], [500, 410], [620, 410],
-            [130, 530], [270, 530], [395, 530], [500, 530], [620, 530],
-        ],
-        'prev_25': [
-            [20, 10], [40, 10], [60, 10], [80, 10], [100, 10],
-            [20, 30], [40, 30], [60, 30], [80, 30], [100, 30],
-            [20, 50], [40, 50], [60, 50], [80, 50], [100, 50],
-            [20, 70], [40, 70], [60, 70], [80, 70], [100, 70],
-            [20, 90], [40, 90], [60, 90], [80, 90], [100, 90]
-        ],
-           }
+    # 'tile_9': [
+    #     [140, 130], [240, 130], [340, 130],
+    #     [140, 230], [240, 230], [340, 230],
+    #     [140, 330], [240, 330], [340, 330]
+    # ],
+    'tile_9': [
+        [370, 210], [460, 210], [550, 210],
+        [370, 290], [460, 290], [550, 290],
+        [370, 380], [460, 380], [550, 380]
+    ],
+    'prev_9': [
+        [10, 10], [30, 10], [50, 10],
+        [10, 30], [30, 30], [50, 30],
+        [10, 50], [30, 50], [50, 50]
+    ],
+    # 'tile_25': [
+    #     [200, 80], [280, 80], [360, 80], [440, 80], [520, 80],
+    #     [200, 155], [280, 155], [360, 155], [440, 155], [520, 155],
+    #     [200, 230], [280, 230], [360, 230], [440, 230], [520, 230],
+    #     [200, 305], [280, 305], [360, 305], [440, 305], [520, 305],
+    #     [200, 380], [280, 380], [360, 380], [440, 380], [520, 380],
+    # ],
+    'tile_25': [
+        [130, 80], [270, 80], [395, 80], [500, 80], [620, 80],
+        [130, 180], [270, 180], [395, 180], [500, 180], [620, 180],
+        [130, 290], [270, 290], [395, 290], [500, 290], [620, 290],
+        [130, 410], [270, 410], [395, 410], [500, 410], [620, 410],
+        [130, 530], [270, 530], [395, 530], [500, 530], [620, 530],
+    ],
+    'prev_25': [
+        [20, 10], [40, 10], [60, 10], [80, 10], [100, 10],
+        [20, 30], [40, 30], [60, 30], [80, 30], [100, 30],
+        [20, 50], [40, 50], [60, 50], [80, 50], [100, 50],
+        [20, 70], [40, 70], [60, 70], [80, 70], [100, 70],
+        [20, 90], [40, 90], [60, 90], [80, 90], [100, 90]
+    ],
+}
+
 
 def colour_detection(h, s, v):
-    #color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
-              #'white': [[180, 18, 255], [0, 0, 231]],
-              #'red1': [[180, 255, 255], [159, 50, 70]],
-              #'red2': [[9, 255, 255], [0, 50, 70]],
-              #'green': [[89, 255, 255], [36, 50, 70]],
-              #'blue': [[128, 255, 255], [90, 50, 70]],
-              #'yellow': [[35, 255, 255], [25, 50, 70]],
-              #'purple': [[158, 255, 255], [129, 50, 70]],
-              #'orange': [[24, 255, 255], [10, 50, 70]],
-              #'gray': [[180, 18, 230], [0, 0, 40]]}
+    # color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
+    # 'white': [[180, 18, 255], [0, 0, 231]],
+    # 'red1': [[180, 255, 255], [159, 50, 70]],
+    # 'red2': [[9, 255, 255], [0, 50, 70]],
+    # 'green': [[89, 255, 255], [36, 50, 70]],
+    # 'blue': [[128, 255, 255], [90, 50, 70]],
+    # 'yellow': [[35, 255, 255], [25, 50, 70]],
+    # 'purple': [[158, 255, 255], [129, 50, 70]],
+    # 'orange': [[24, 255, 255], [10, 50, 70]],
+    # 'gray': [[180, 18, 230], [0, 0, 40]]}
     #print([h, s, v])
-    if(h>=0 and h <9) and (s>=50 and s<=255) and (v>=70 and v<=255):
+    if(h >= 0 and h < 9) and (s >= 50 and s <= 255) and (v >= 70 and v <= 255):
         return 'orange'
-    elif(h>=9 and h <=20) and (s>=50 and s<=255) and (v>=70 and v<=255):
+    elif(h >= 9 and h <= 20) and (s >= 50 and s <= 255) and (v >= 70 and v <= 255):
         return 'orange'
-    #elif(h>=0 and h <=180) and (s>=0 and s<=18) and (v>=170 and v<=255):
-    elif(h>=0 and h <=180) and (s>=0 and s<=35) and (v>=140 and v<=255):
+    # elif(h>=0 and h <=180) and (s>=0 and s<=18) and (v>=170 and v<=255):
+    elif(h >= 0 and h <= 180) and (s >= 0 and s <= 35) and (v >= 140 and v <= 255):
         return 'white'
-    elif(h>20 and h <=35) and (s>=50 and s<=255):
+    elif(h > 20 and h <= 35) and (s >= 50 and s <= 255):
         return 'yellow'
-    elif(h>=39 and h <=89) and (s>=50 and s<=255):
+    elif(h >= 39 and h <= 89) and (s >= 50 and s <= 255):
         return 'green'
-    elif(h>=90 and h <=128) and (s>=50 and s<=255) and (v>=70 and v<=255):
+    elif(h >= 90 and h <= 128) and (s >= 50 and s <= 255) and (v >= 70 and v <= 255):
         return 'blue'
-    elif(h>=159 and h <=180) and (s>=50 and s<=255):
+    elif(h >= 159 and h <= 180) and (s >= 50 and s <= 255):
         return 'red'
     else:
         return 'black'
 
-def draw(image,sticker_name):
+
+def draw(image, sticker_name):
     for x, y in stickers[sticker_name]:
         cv2.rectangle(image, (x, y), (x + 10, y + 10), (255, 255, 0), 2)
 
+
 def draw_tile(tile, image, sticker_name):
     for i in range(tile):
-        hsv.append(image[stickers[sticker_name][i][1] + 10][stickers[sticker_name][i][0] + 10])
+        hsv.append(image[stickers[sticker_name][i][1] + 10]
+                   [stickers[sticker_name][i][0] + 10])
+
 
 def draw_prev(image, sticker_name):
-    a= 0
+    a = 0
     for x, y in stickers[sticker_name]:
         colour = colour_detection(hsv[a][0], hsv[a][1], hsv[a][2])
         cv2.rectangle(image, (x, y), (x + 10, y + 10), color[colour], -1)
-        a+=1
+        a += 1
         if(sticker_name == 'prev_9'):
             color_tile9.append(colour)
         elif(sticker_name == 'prev_25'):
@@ -122,9 +132,10 @@ def draw_prev(image, sticker_name):
 
 ###########################################################
 
+
 # Create the constants (go ahead and experiment with different values)
 BOARDWIDTH = 5  # number of columns in the board
-BOARDHEIGHT = 5 # number of rows in the board
+BOARDHEIGHT = 5  # number of rows in the board
 TILESIZE = 80
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
@@ -132,12 +143,12 @@ FPS = 30
 BLANK = None
 
 #                 R    G    B
-BLACK =         (  0,   0,   0)
-WHITE =         (255, 0, 255)
-BRIGHTBLUE =    (  0,  50, 255)
-DARKTURQUOISE = (  3,  54,  73)
+BLACK = (0,   0,   0)
+WHITE = (255, 0, 255)
+BRIGHTBLUE = (0,  50, 255)
+DARKTURQUOISE = (3,  54,  73)
 #DARKTURQUOISE = (  0,   0,   0)
-GREEN =         (  0, 204,   0)
+GREEN = (0, 204,   0)
 
 BGCOLOR = DARKTURQUOISE
 TILECOLOR = GREEN
@@ -150,7 +161,8 @@ BUTTONTEXTCOLOR = BLACK
 MESSAGECOLOR = WHITE
 
 XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
-YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
+YMARGIN = int(
+    (WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
 
 UP = 'up'
 DOWN = 'down'
@@ -158,6 +170,7 @@ LEFT = 'left'
 RIGHT = 'right'
 
 flag_status = 0
+
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
@@ -169,14 +182,18 @@ def main():
     BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
 
     # Store the option buttons and their rectangles in OPTIONS.
-    RESET_SURF, RESET_RECT = makeText('Reset',    TEXTCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
-    NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
-    SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXTCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
+    RESET_SURF, RESET_RECT = makeText(
+        'Reset',    TEXTCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
+    NEW_SURF,   NEW_RECT = makeText(
+        'New Game', TEXTCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
+    SOLVE_SURF, SOLVE_RECT = makeText(
+        'Solve',    TEXTCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
 
     mainBoard, solutionSeq = generateNewPuzzle(1)
-    SOLVEDBOARD = getSolvedBoard() # a solved board is the same as the board in a start state.
-    allMoves = [] # list of moves made from the solved configuration
-    print('mainBoard' , mainBoard)
+    # a solved board is the same as the board in a start state.
+    SOLVEDBOARD = getSolvedBoard()
+    allMoves = []  # list of moves made from the solved configuration
+    print('mainBoard', mainBoard)
     print('solvedBoard', SOLVEDBOARD)
 
     # while True: # main game loop
@@ -241,32 +258,46 @@ def terminate():
 
 
 def checkForQuit():
-    for event in pygame.event.get(QUIT): # get all the QUIT events
-        terminate() # terminate if any QUIT events are present
-    for event in pygame.event.get(KEYUP): # get all the KEYUP events
+    for event in pygame.event.get(QUIT):  # get all the QUIT events
+        terminate()  # terminate if any QUIT events are present
+    for event in pygame.event.get(KEYUP):  # get all the KEYUP events
         if event.key == K_ESCAPE:
-            terminate() # terminate if the KEYUP event was for the Esc key
-        pygame.event.post(event) # put the other KEYUP event objects back
+            terminate()  # terminate if the KEYUP event was for the Esc key
+        pygame.event.post(event)  # put the other KEYUP event objects back
 
 
 def getStartingBoard():
     # Return a board data structure with tiles in the solved state.
     # For example, if BOARDWIDTH and BOARDHEIGHT are both 3, this function
     # returns [[1, 4, 7], [2, 5, 8], [3, 6, BLANK]]
-    board = []; column = []; counter = 1
-    for i in range(1,(BOARDHEIGHT * BOARDWIDTH) + 1):
-        if(color_tile25[i-1] == 'black'): column.append(BLANK)
-        else: column.append(counter); counter += 1
-        if(i%5 == 0): board.append(column); column = []
+    board = []
+    column = []
+    counter = 1
+    for i in range(1, (BOARDHEIGHT * BOARDWIDTH) + 1):
+        if(color_tile25[i-1] == 'black'):
+            column.append(BLANK)
+        else:
+            column.append(counter)
+            counter += 1
+        if(i % 5 == 0):
+            board.append(column)
+            column = []
     return board
 
+
 def getSolvedBoard():
-    solvedBoard = []; solvedColumn = []; solvedcounter = 1
-    for i in range(1,(BOARDHEIGHT * BOARDWIDTH) + 1):
-        solvedColumn.append(solvedcounter); solvedcounter += 1
-        if(i%5 == 0): solvedBoard.append(solvedColumn); solvedColumn = []
+    solvedBoard = []
+    solvedColumn = []
+    solvedcounter = 1
+    for i in range(1, (BOARDHEIGHT * BOARDWIDTH) + 1):
+        solvedColumn.append(solvedcounter)
+        solvedcounter += 1
+        if(i % 5 == 0):
+            solvedBoard.append(solvedColumn)
+            solvedColumn = []
     solvedBoard[BOARDHEIGHT - 1][BOARDWIDTH - 1] = BLANK
     return solvedBoard
+
 
 def getBlankPosition(board):
     # Return the x and y of board coordinates of the blank space.
@@ -281,14 +312,19 @@ def makeMove(board, move):
     blankx, blanky = getBlankPosition(board)
 
     if move == UP:
-        board[blankx][blanky], board[blankx - 1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]
+        board[blankx][blanky], board[blankx -
+                                     1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]
     elif move == DOWN:
-        board[blankx][blanky], board[blankx + 1][blanky] = board[blankx + 1][blanky], board[blankx][blanky]
+        board[blankx][blanky], board[blankx +
+                                     1][blanky] = board[blankx + 1][blanky], board[blankx][blanky]
     elif move == LEFT:
-        board[blankx][blanky], board[blankx][blanky - 1] = board[blankx][blanky - 1], board[blankx][blanky]
+        board[blankx][blanky], board[blankx][blanky -
+                                             1] = board[blankx][blanky - 1], board[blankx][blanky]
     elif move == RIGHT:
-        board[blankx][blanky], board[blankx][blanky + 1] = board[blankx][blanky + 1], board[blankx][blanky]
+        board[blankx][blanky], board[blankx][blanky +
+                                             1] = board[blankx][blanky + 1], board[blankx][blanky]
     print(board)
+
 
 def isValidMove(board, move):
     blankx, blanky = getBlankPosition(board)
@@ -299,7 +335,7 @@ def isValidMove(board, move):
 
 
 # def getRandomMove():
-#     return 
+#     return
 
 
 def getLeftTopOfTile(tileX, tileY):
@@ -324,12 +360,15 @@ def drawTile(tilex, tiley, number, adjx=0, adjy=0):
     # pixels over (determined by adjx and adjy)
     left, top = getLeftTopOfTile(tilex, tiley)
     if flag_status == 0:
-        pygame.draw.rect(DISPLAYSURF, color_tile25[number - 1], (left + adjx, top + adjy, TILESIZE, TILESIZE))
+        pygame.draw.rect(
+            DISPLAYSURF, color_tile25[number - 1], (left + adjx, top + adjy, TILESIZE, TILESIZE))
     elif flag_status == 1:
-        pygame.draw.rect(DISPLAYSURF, color_tile25[number], (left + adjx, top + adjy, TILESIZE, TILESIZE))
+        pygame.draw.rect(
+            DISPLAYSURF, color_tile25[number], (left + adjx, top + adjy, TILESIZE, TILESIZE))
     textSurf = BASICFONT.render(str(number), True, TEXTCOLOR)
     textRect = textSurf.get_rect()
-    textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
+    textRect.center = left + int(TILESIZE / 2) + \
+        adjx, top + int(TILESIZE / 2) + adjy
     DISPLAYSURF.blit(textSurf, textRect)
 
 
@@ -339,6 +378,7 @@ def makeText(text, color, top, left):
     textRect = textSurf.get_rect()
     textRect.topleft = (top, left)
     return (textSurf, textRect)
+
 
 def drawBoard(board, message):
     global flag_status
@@ -354,11 +394,11 @@ def drawBoard(board, message):
                     flag_status = 1
                 drawTile(tilex, tiley, board[tiley][tilex])
 
-
     left, top = getLeftTopOfTile(0, 0)
     width = BOARDWIDTH * TILESIZE
     height = BOARDHEIGHT * TILESIZE
-    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4)
+    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5,
+                     top - 5, width + 11, height + 11), 4)
 
     DISPLAYSURF.blit(RESET_SURF, RESET_RECT)
     DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
@@ -387,7 +427,8 @@ def slideAnimation(board, direction, message, animationSpeed):
     baseSurf = DISPLAYSURF.copy()
     # draw a blank space over the moving tile on the baseSurf Surface.
     moveLeft, moveTop = getLeftTopOfTile(movex, movey)
-    pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft, moveTop, TILESIZE, TILESIZE))
+    pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft,
+                     moveTop, TILESIZE, TILESIZE))
 
     for i in range(0, TILESIZE, animationSpeed):
         # animate the tile sliding over
@@ -411,18 +452,18 @@ def generateNewPuzzle(numSlides):
     # animate these moves).
     sequence = []
     board = getStartingBoard()
-    print(board)    
+    print(board)
     drawBoard(board, '')
     pygame.display.update()
-    pygame.time.wait(500) # pause 500 milliseconds for effect
-    #Get Randomizer here
-    #Randomizer() func
+    pygame.time.wait(500)  # pause 500 milliseconds for effect
+    # Get Randomizer here
+    # Randomizer() func
     return board, sequence
 
 
 def resetAnimation(board, allMoves):
     # make all of the moves in allMoves in reverse.
-    revAllMoves = allMoves[:] # gets a copy of the list
+    revAllMoves = allMoves[:]  # gets a copy of the list
     print(revAllMoves)
     revAllMoves.reverse()
 
@@ -435,10 +476,12 @@ def resetAnimation(board, allMoves):
             oppositeMove = LEFT
         elif move == LEFT:
             oppositeMove = RIGHT
-        slideAnimation(board, oppositeMove, '', animationSpeed=int(TILESIZE / 2))
+        slideAnimation(board, oppositeMove, '',
+                       animationSpeed=int(TILESIZE / 2))
         makeMove(board, oppositeMove)
 
 ##########################################################
+
 
 if __name__ == '__main__':
     img9 = cv2.imread("Images/9_tile_2.png")
@@ -459,9 +502,9 @@ if __name__ == '__main__':
 
     cv2.imshow("Goal State Image", img9[0:900, 0:900])
     cv2.imshow("Pattern Image", img25[0:900, 0:900])
-    
-    print("Tile_9",color_tile9)
-    print("Tile_25",color_tile25)
+
+    print("Tile_9", color_tile9)
+    print("Tile_25", color_tile25)
 
     main()
 
@@ -470,5 +513,3 @@ if __name__ == '__main__':
 
 
 ###########################################################################
-
-
